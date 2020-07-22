53cc220b85a80afce72ea2916a9650f78109e4da
Vedran Vukovic <vedran@outlook>
2020-07-22 21:28:01 +0200

    Chaning color for users online, splitting the info into two divs, putting check message if there is no one to print out

--- src/components/UsersOnlineComponent.jsx
+++ src/components/UsersOnlineComponent.jsx
@@ -2,16 +2,23 @@ import React from 'react';
 
 export default function UsersOnline({ membersOnline }) {
     return (
-        <div className="alert alert-dark" role="alert">
-            <strong>Broj korisnika online:</strong> {membersOnline.length}<br />
-            <strong>Lista korisnika:</strong><br />
-            <div>
-                {membersOnline.map((m, idx) => {
-                    return (
-                    <span key={idx} className={m.clientData.color} >{m.clientData.username}<br /></span>
-                    )
-                })}
+        <div className="container" >
+            <div className="row alert alert-info" role="alert">
+                <div className="col-md-6">
+                    <strong>Lista korisnika:</strong>&nbsp;{(membersOnline.length === 0) ? "Trenutno nema nikoga" : ""}<br />
+                    <div>
+                        {membersOnline.map((m, idx) => {
+                            return (
+                                <span key={idx}><i>{m.clientData.username}</i><br /></span>
+                            )
+                        })}
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <strong>Broj korisnika online:</strong> {membersOnline.length}<br />
+                </div>
             </div>
         </div>
+
     )
 }
